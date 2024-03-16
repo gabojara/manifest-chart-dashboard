59f9e5513331cf02110aa890099cfa531172546b
jeevan <122259771+Unicorn8080@users.noreply.github.com>
2024-03-17 04:49:38 +0900

    feat: add rough responsive to dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -3,30 +3,30 @@ import ChartCard from '../components/card/chartCard'
 
 export const Dashboard = () => {
   return (
-    <div className="w-full bg-[#FFFDF4] p-[100px]">
-      <div className="flex items-center justify-between">
-        <span className="py-40 font-rubik text-[40px] font-bold leading-[100%]">
+    <div className="w-full bg-[#FFFDF4] p-[100px] px-24 mobile:px-32 tablet:px-64 desktop-s:px-[100px]">
+      <div className="flex w-full flex-col items-center justify-start py-[20px] desktop:flex-row desktop:justify-between">
+        <span className="self-start py-40 font-rubik text-[24px] font-bold leading-[0%] mobile:text-[32px] tablet:text-[40px] desktop:self-center">
           Manifest Analytics Dashboard
         </span>
-        <div className="flex items-center gap-8">
-          <span className="pr-12 font-rubik text-[16px] font-medium uppercase">
+        <div className="flex max-w-full items-center gap-8 self-end desktop:self-center">
+          <span className="pr-12 font-rubik text-[14px] font-medium uppercase tablet:text-[16px]">
             Filter By
           </span>
-          <button className="w-[96px] rounded-[8px] border border-[#C9CED1] bg-white px-12 py-8 text-left text-[16px] leading-[140%] text-[#6F7274]">
+          <button className="w-64 rounded-[8px] border border-[#C9CED1] bg-white px-12 py-8 text-left text-[14px] leading-[140%] text-[#6F7274] tablet:w-[96px] tablet:text-[16px]">
             Age
           </button>
-          <button className="w-[96px] rounded-[8px] border border-[#C9CED1] bg-white px-12 py-8 text-left text-[16px] leading-[140%] text-[#6F7274]">
+          <button className="w-64 rounded-[8px] border border-[#C9CED1] bg-white px-12 py-8 text-left text-[14px] leading-[140%] text-[#6F7274] tablet:w-[96px] tablet:text-[16px]">
             Grade
           </button>
-          <button className="w-[96px] rounded-[8px] border border-[#C9CED1] bg-white px-12 py-8 text-left text-[16px] leading-[140%] text-[#6F7274]">
+          <button className="w-64 rounded-[8px] border border-[#C9CED1] bg-white px-12 py-8 text-left text-[14px] leading-[140%] text-[#6F7274] tablet:w-[96px] tablet:text-[16px]">
             Gender
           </button>
-          <button className="w-[96px] rounded-[8px] border border-[#C9CED1] bg-white px-12 py-8 text-left text-[16px] leading-[140%] text-[#6F7274]">
+          <button className="w-64 rounded-[8px] border border-[#C9CED1] bg-white px-12 py-8 text-left text-[14px] leading-[140%] text-[#6F7274] tablet:w-[96px] tablet:text-[16px]">
             School
           </button>
         </div>
       </div>
-      <div className="flex gap-[20px]">
+      <div className="hidden w-full gap-[20px] desktop-s:flex">
         <div className="grid flex-1 grid-cols-2 gap-[20px]">
           <ChartCard className="col-span-2 w-full">
             <Chart.ActiveStudents />
@@ -39,7 +39,7 @@ export const Dashboard = () => {
           <Chart.HeatMapPeakUsageTime className="col-span-2 w-full" />
           <Chart.ColumnTopTopics className="col-span-2 w-full" />
         </div>
-        <div className="grid flex-1 grid-cols-2 gap-[20px]">
+        <div className="grid h-fit flex-1 grid-cols-2 gap-[20px]">
           <ChartCard className="col-span-1 w-full">
             <Chart.DountGender />
           </ChartCard>
@@ -52,6 +52,29 @@ export const Dashboard = () => {
           <Chart.EfficacyMeasurements className="col-span-2 w-full" />
           <Chart.DountUserSentimentOverview className="col-span-2 w-full" />
         </div>
+      </div>
+      <div className="grid w-full flex-1 grid-cols-2 gap-[20px] desktop-s:hidden">
+        <ChartCard className="col-span-2 w-full">
+          <Chart.ActiveStudents />
+        </ChartCard>
+        <ChartCard className="col-span-1 w-full">
+          <Chart.DountGender />
+        </ChartCard>
+        <ChartCard className="col-span-1 w-full">
+          <Chart.DountAge />
+        </ChartCard>
+        <ChartCard className="col-span-2 w-full">
+          <Chart.AreaTotalSessions />
+        </ChartCard>
+        <ChartCard className="col-span-2 w-full">
+          <Chart.Grade />
+        </ChartCard>
+        <Chart.AreaSessionLength className="col-span-1 w-full" />
+        <Chart.ColumnAverageSession className="col-span-1 w-full" />
+        <Chart.EfficacyMeasurements className="col-span-2 w-full" />
+        <Chart.HeatMapPeakUsageTime className="col-span-2 w-full" />
+        <Chart.ColumnTopTopics className="col-span-2 w-full" />
+        <Chart.EfficacyMeasurements className="col-span-2 w-full" />
       </div>
     </div>
   )
